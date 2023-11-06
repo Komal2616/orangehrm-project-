aeb765327a76b4b974b6c3f89da1119035b7a3a1
Komal2616 <72749644+Komal2616@users.noreply.github.com>
2023-11-06 23:53:25 +0530

    Update PIMPage.java

--- PIMPage.java
+++ PIMPage.java
@@ -1,4 +1,11 @@
 package pageEvents;
+import java.util.List;
+
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 public class PIMPage {
 	
